42a3f061142cbff378624149da116e35bedbe198
ascensus <ascensus@apollo.local.localdomain>
2025-09-08 07:10:59 -0400

    New stuff

--- bootc-buildiso/buildiso.sh
+++ bootc-buildiso/buildiso.sh
@@ -1,26 +1,27 @@
-#sudo podman pull localhost:5000/rhel10-bootc-wks:latest
+sudo podman pull --tls-verify=false localhost:5000/rhel10-bootc-wks:latest
 #
-#sudo podman run --rm -it \
-#    --privileged \
-#    --pull=newer \
-#    --security-opt label=type:unconfined_t \
-#    -v $(pwd)/output:/output \
-#    -v /var/lib/containers/storage:/var/lib/containers/storage \
-#    quay.io/centos-bootc/bootc-image-builder:latest \
-#    build \
-#    --type iso \
-#    localhost:5000/rhel10-bootc-wks:latest
-
-
-sudo podman pull localhost:5000/rhel9-bootc-scapped-v1.1:latest
-
 sudo podman run --rm -it \
-	--privileged --security-opt label=type:unconfined_t \
-	--authfile auth.json \
-	-v /var/lib/containers/storage:/var/lib/containers/storage \
-	-v "$(pwd)/output:/output" \
-	-v ~/bootc-project/bootc-buildiso/config.toml:/config.toml quay.io/centos-bootc/bootc-image-builder \
-	build \
-	--type iso \
-	--config /config.toml \
-	localhost:5000/rhel9-bootc-scapped-v1.1:latest
+    --privileged \
+    --pull=newer \
+    --authfile auth.json \
+    --security-opt label=type:unconfined_t \
+    -v $(pwd)/output:/output \
+    -v /var/lib/containers/storage:/var/lib/containers/storage:Z \
+     registry.redhat.io/rhel10/bootc-image-builder:latest \
+    build \
+    --type iso \
+    localhost:5000/rhel10-bootc-wks:latest
+
+
+#sudo podman pull localhost:5000/rhel9-bootc-scapped-v1.1:latest
+
+#sudo podman run --rm -it \
+#	--privileged --security-opt label=type:unconfined_t \
+#	--authfile auth.json \
+#	-v /var/lib/containers/storage:/var/lib/containers/storage \
+#	-v "$(pwd)/output:/output" \
+#	-v ~/bootc-project/bootc-buildiso/config.toml:/config.toml quay.io/centos-bootc/bootc-image-builder \
+#	build \
+#	--type iso \
+#	--config /config.toml \
+#	localhost:5000/rhel9-bootc-scapped-v1.1:latest
